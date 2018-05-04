3223f1836635a484867aa3f2cae61e8abb9903ec
danielss24 <daniels_s_24@hotmail.com>
2018-05-04 14:52:20 +0200

    Proyecto SOPER, falta gestor de apuestas y retoques

---
+++ Proyecto/testMem1.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <unistd.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/mman.h>
+#include <wait.h>
+
+/* Para usar fstat */
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include <syslog.h>
+
+#include "semaforos.h"
+#include "utilidades.h"
+#include "caballo.h"
+#include "shared_memory.h"
+#include "gestor_apuestas.h"
+#include "imprimir.h"
+
+int main(){
+
+	estructura_memoria_compartida * mem_compartida;
+
+	int * semaforo;	
+	unsigned short array_comun[2] = {1, 0}; /*1 semaforo,inicializados a 0*/
+	int id = 0;
+		
+	semaforo = (int*) malloc(sizeof (int)); 
+    if (semaforo == NULL) {printf("Linea %d - Error al reservar memoria\n", __LINE__);}
+    if (-1 == Crear_Semaforo(IPC_PRIVATE, 1, semaforo)) {printf("Linea %d - Error al crear el semaforo\n", __LINE__);return -1;}
+    if (Inicializar_Semaforo(*semaforo, array_comun) == -1) {printf("\n Linea %d - Error al inicializar el semaforo\n", __LINE__);return -1;} 
+
+    if((id=shmget(123,sizeof(struct _estructura_memoria_compartida),IPC_CREAT|IPC_EXCL|0660))==-1){
+		if((id=shmget(123,sizeof(struct _estructura_memoria_compartida),0))==-1){
+			printf("Error al abrir el segmento\n");
+		}
+		mem_compartida = shmat (id, (char *)0, 0);
+		if (mem_compartida == NULL) {
+			return -1; 
+		}
+	}
+
+	mem_compartida = shmat (id, (char *)0, 0);
+	if (mem_compartida == NULL) {
+		fprintf (stderr, "Error reserve shared memory \n");
+		return -1; 
+	}
+	mem_compartida->semaforo_id = * semaforo;
+
+
+	gotoxy(50,50);
+	printf("diosaioasdasofass\n");
+
+
+	set_caballos_num_caballos(&mem_compartida->caballos_creados,10);
+	inicializar_caballos(mem_compartida->caballos_creados);
+
+	set_caballos_id(&mem_compartida->caballos_creados,1,5);
+	printf("dsadasdas %d\n", mem_compartida->semaforo_id);
+
+	Down_Semaforo(mem_compartida->semaforo_id,0,SEM_UNDO);
+
+	printf("ID %d\n", get_caballos_id(mem_compartida->caballos_creados,1));
+	printf("Num %d\n", get_caballos_num_caballos(mem_compartida->caballos_creados));
+
+
+	//shmctl(id,IPC_SET,(struct shmid_ds *)caballos_creados);	
+
+
+	///////////////////////////
+
+
+
+	//shmctl(mem_id,IPC_SET,caballos_creados);
+
+}
